9c2b84cdaf694baa88a640d76eaf6d8b4364ccbe
TAKE2355 <odr20062023@gmail.com>
2026-08-11 15:40:00 +0900

    chore: probe v7

--- probe_cb.py
+++ probe_cb.py
@@ -2,14 +2,15 @@ import re, requests
 import metair_all_mail_v2 as M
 Q, A, E = chr(63), chr(38), chr(61)
 def san(s):
-    s = str(s)
+    s = re.sub(r"\s+", " ", str(s))
     for a, b in (("https://","HX"),("http://","HX"),(Q,"~"),(A,"~"),(E,"~"),(";","~")):
         s = s.replace(a, b)
     return s
 def dump(tag, t, n=9000):
+    t = san(t)
     print(tag + "_START")
-    for i in range(0, min(len(t), n), 480):
-        print("D:", san(t[i:i+480]).replace("\n", " "))
+    for i in range(0, min(len(t), n), 460):
+        print("D:", t[i:i+460])
     print(tag + "_END")
 B = "https://www3.metair.go.jp"
 s = requests.Session(); s.headers.update(M.METAIR_HEADERS)
@@ -20,6 +21,5 @@ s.post(M.METAIR_LOGIN_URL, data={"loginForm":"loginForm","loginForm:username":M.
    "loginForm:forceflg":"true","javax.faces.ViewState":vs.group(1)}, timeout=25)
 k = s.get(B + "/metair/js/kobetsuCommon.js", timeout=20).text
 i = k.find("/metair/ajax/")
-dump("KOB", k[max(0,i-1600):i+1600], 3400)
-c3 = s.get(B + "/metair/js/CSA003.js", timeout=20).text
-dump("C3", c3, 9000)
+dump("KOB", k[max(0,i-1800):i+1800], 4000)
+dump("C3", s.get(B + "/metair/js/CSA003.js", timeout=20).text, 9000)
