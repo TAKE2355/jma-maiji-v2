5cec7c413f9844c7c4061087b448aebb6b8f6163
TAKE2355 <odr20062023@gmail.com>
2026-08-11 15:37:16 +0900

    chore: probe v5

--- probe_cb.py
+++ probe_cb.py
@@ -1,4 +1,4 @@
-import os, re, requests
+import re, requests
 import metair_all_mail_v2 as M
 Q, A, E = chr(63), chr(38), chr(61)
 def san(s):
@@ -7,21 +7,23 @@ def san(s):
         s = s.replace(a, b)
     return s
 B = "https://www3.metair.go.jp"
-s = requests.Session()
-s.headers.update(M.METAIR_HEADERS)
+s = requests.Session(); s.headers.update(M.METAIR_HEADERS)
 r0 = s.get(M.METAIR_LOGIN_URL, timeout=20)
 vs = re.search(r'name="javax\.faces\.ViewState"[^>]+value="([^"]+)"', r0.text)
-print("VS", bool(vs))
 data = {"loginForm":"loginForm","loginForm:username":M.METAIR_USER,
         "loginForm:password":M.METAIR_PASS,"loginForm:doLogin":"\u30ed\u30b0\u30a4\u30f3",
-        "loginForm:forceflg":"true","javax.faces.ViewState":vs.group(1) if vs else ""}
-r1 = s.post(M.METAIR_LOGIN_URL, data=data, timeout=25, allow_redirects=True)
-print("LOGIN", r1.status_code, san(r1.url)[-46:], len(r1.text))
-u = B + "/metair/view/winKobetsu/CSA003.html" + Q + "csid" + E + "CSA003_KUMO" + A + "type" + E + "2"
-r = s.get(u, timeout=30)
-h = r.text
-print("PAGELEN", r.status_code, san(r.url)[-46:], len(h))
-print("DUMP_START")
-for i in range(0, min(len(h), 9000), 480):
-    print("D:", san(h[i:i+480]).replace("\n", " "))
-print("DUMP_END")
+        "loginForm:forceflg":"true","javax.faces.ViewState":vs.group(1)}
+s.post(M.METAIR_LOGIN_URL, data=data, timeout=25)
+for j in ["/metair/js/CSA003.js", "/metair/js/common/CSAcommon.js", "/metair/js/common/baseImage.js", "/metair/js/kobetsuCommon.js", "/metair/js/singleImageFunc.js"]:
+    try:
+        r = s.get(B + j, timeout=20)
+        t = r.text
+        print("JS", san(j), r.status_code, len(t))
+        if r.status_code != 200: continue
+        for pat, tag in [(r"/pict/[^\"\'\s]*", "PICT"), (r"ajax[A-Za-z0-9_]*", "AJAX"), (r"csid[^\"\'\s,)]*", "CSID"), (r"[A-Z]{4}[0-9]{2}", "CODE")]:
+            v = sorted(set(re.findall(pat, t)))[:40]
+            if v: print("   " + tag + ":", [san(x) for x in v])
+        for m in re.finditer(r"(url|src|path|dir)\s*[:=]\s*[\"\'][^\"\']{4,120}[\"\']", t):
+            print("   U:", san(m.group(0))[:150])
+    except Exception as e:
+        print("JSERR", san(j), e)
