e762533431e0e56047bf85c733f00e59b67a5290
TAKE2355 <odr20062023@gmail.com>
2026-08-11 15:32:29 +0900

    chore: probe v2

--- probe_cb.py
+++ probe_cb.py
@@ -1,57 +1,30 @@
-import os, re, sys, datetime, requests
+import os, re, requests
 import metair_all_mail_v2 as M
-
+Q, A, E = chr(63), chr(38), chr(61)
 def san(s):
     s = str(s)
-    for a, b in (("https://","HX"),("http://","HX"),("?","~"),("&","~"),("=","~"),(";","~")):
+    for a, b in (("https://","HX"),("http://","HX"),(Q,"~"),(A,"~"),(E,"~"),(";","~")):
         s = s.replace(a, b)
     return s
-
-H = M.METAIR_HEADERS
-try:
-    S = M.get_metair_session()
-except Exception as e:
-    S = None
-    print("SESSERR", e)
-get = (S.get if S else requests.get)
-
-PAGES = [
-    "https://www3.metair.go.jp/metair/view/winKobetsu/CSA003.html" + chr(63) + "csid" + chr(61) + "CSA003_KUMO" + chr(38) + "type" + chr(61) + "2",
-    "https://www3.metair.go.jp/metair/view/start/XS001.html",
-]
-for u in PAGES:
-    try:
-        r = get(u, headers=H, timeout=30)
-        h = r.text
-        print("PAGE", san(u)[-40:], r.status_code, len(h))
-        for m in re.finditer("積乱雲", h):
-            print("  CB:", san(h[max(0,m.start()-250):m.start()+250]).replace("\n"," ")[:460])
-        print("  PICT:", sorted(set(san(x) for x in re.findall(r"/pict/[A-Za-z0-9_/\-]*", h)))[:60])
-        print("  CSID:", sorted(set(re.findall(r"CSA[0-9]{3}_[A-Z0-9]+", h)))[:60])
-        print("  ENJP:", sorted(set(re.findall(r"[A-Z]{4}[0-9]{2}_RJTD", h)))[:60])
-    except Exception as e:
-        print("PAGEERR", san(u)[-40:], e)
-
-# 候補ディレクトリ／プレフィックスの総当たり
-now = datetime.datetime.utcnow()
-DIRS = ["alt","cb","cba","cbarea","rdca","cbtop","conv","cumulonimbus","sekiran"]
-PFX  = ["ENJP60","ENJP61","ENJP62","ENJP63","ENJP64","ENJP65","ENJP66","ENJP67","ENJP68"]
-found = []
-for d in DIRS:
-    for p in PFX:
-        hit = False
-        for back in range(0, 8):
-            t = now - datetime.timedelta(minutes=5*back)
-            t = t.replace(minute=(t.minute//5)*5, second=0, microsecond=0)
-            u = "https://www3.metair.go.jp/pict/satellite/hf/%s/%s_RJTD_%s00.jpg" % (d, p, t.strftime("%Y%m%d%H%M"))
-            try:
-                rr = requests.head(u, headers=H, timeout=6)
-                if rr.status_code == 200:
-                    found.append("%s / %s / %s" % (d, p, t.strftime("%Y%m%d%H%M")))
-                    hit = True
-                    break
-            except Exception:
-                pass
-        if hit:
-            print("HIT", d, p)
-print("FOUND", found)
+S = M.get_metair_session()
+print("SESSION", "OK" if S else "NONE")
+B = "https://www3.metair.go.jp"
+u = B + "/metair/view/winKobetsu/CSA003.html" + Q + "csid" + E + "CSA003_KUMO" + A + "type" + E + "2"
+r = S.get(u, timeout=30)
+h = r.text
+print("PAGELEN", r.status_code, len(h))
+print("DUMP_START")
+for i in range(0, min(len(h), 7000), 500):
+    print("D:", san(h[i:i+500]).replace("\n", " "))
+print("DUMP_END")
+for path in ["/metair/ajax/CSA003/ajaxUpdate", "/metair/ajax/CSA003/ajaxInit"]:
+    for qs in [Q+"csid"+E+"CSA003_KUMO"+A+"type"+E+"2",
+               Q+"contentsType"+E+"0"+A+"csid"+E+"CSA003_KUMO"+A+"type"+E+"2"+A+"lastDate"+E,
+               Q+"csid"+E+"CSA003_CB"+A+"type"+E+"2"]:
+        try:
+            rr = S.get(B + path + qs, timeout=20)
+            print("AJAX", san(path), san(qs)[:60], rr.status_code, len(rr.text))
+            if rr.status_code == 200 and len(rr.text) < 4000:
+                print("   BODY:", san(rr.text).replace("\n", " ")[:900])
+        except Exception as e:
+            print("AJAXERR", san(path), e)
