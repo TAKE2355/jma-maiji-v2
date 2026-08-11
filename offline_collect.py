039079f6bfaa55d712137ba113d5ea5956b92d44
TAKE2355 <odr20062023@gmail.com>
2026-08-11 19:27:05 +0900

    feat: thunder series

--- offline_collect.py
+++ offline_collect.py
@@ -182,6 +182,10 @@ def collect_series(s, jma_ts=None, akuten_ts=None):
         tt = M.nowc_times()[:n]
         specs = [(e.get("validtime"), e.get("basetime"), None) for e in tt]
         M._nowc_basemap()          # 地図タイルを先に1回だけ作ってスレッド間で共有
+    elif kind == "thnc":
+        sp = M.nowc_thunder_specs(n)
+        specs = [(vt, (bt, vt2, lbt, lvt), None) for vt, bt, vt2, lbt, lvt in sp]
+        M._nowc_basemap()
     elif kind == "pagasa":
         fr = M._pagasa_frames()
         view = M.PAGASA_VIEWS.get(str(s.get("code")))
@@ -196,6 +200,8 @@ def collect_series(s, jma_ts=None, akuten_ts=None):
                 im = M._pagasa_compose(url, view)
             elif kind == "nowc":
                 im, _ts = M.get_nowc_hrpns(url, ts)
+            elif kind == "thnc":
+                im, _ts = M.get_nowc_thunder(*url)
             else:
                 b = get_bytes(url, hdr)
                 im = Image.open(io.BytesIO(b)) if b else None
